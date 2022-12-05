e2ec7bfc3849cb0a3072d49d4c0b8428ff361cdb
Danil <nnaumov538@gmail.com>
2022-12-05 11:20:03 +0500

    Add code

---
+++ Gachi.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+password = input("\nGive your password \n\n>>> ")
+
+if password == "world cum shot":
+	worldcs = input("\nWcs ? \n\n>>> ")
+	if worldcs == "yes" or "Yes" or "YES":
+		vremya = datetime.now().hour
+		if vremya >= 7 and 23:
+			print("Now " + str(vremya) + " hours, mef day, aeeee)))0))0))")
+		else:
+			print("\nSpim ZzZz..."):
+				exit()
